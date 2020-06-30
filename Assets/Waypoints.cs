c0fa8ab1a3f6a012e6042f034e6335471d1bc0fa
Tanvir Ahmed <tan.ahmed98@gmail.com>
2020-06-30 21:56:35 +0600

    Initial commit

---
+++ Assets/Waypoints.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    public GameObject player;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
+    void Update()
+    {
+        if (current <= waypoints.Length)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+            RotateToTarget(waypoints[current].transform);
+        }
+       
+    }
+
+   
+
+    void OnTriggerEnter(Collider n)
+    {
+       
+        if (n.gameObject.tag == "waypoint")
+        {
+            Debug.Log("collide 2");
+            current++;
+            Debug.Log(current);
+        }
+    }
+    void RotateToTarget(Transform target)
+    {
+        Vector3 targetDirection = target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = speed * 2 * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Draw a ray pointing at our target in
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+        
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BigBlue_nextGen
+ Worked on the 2nd part of youtube short video with cinemachine and timeline
